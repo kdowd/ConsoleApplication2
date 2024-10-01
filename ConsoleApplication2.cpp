3b7e4ce1bcc97bdd60bf7978d053f8fa1f5007d8
kdowd <kevin.dowd@gmail.com>
2024-10-01 18:14:55 +1300

    Add project files.

---
+++ ConsoleApplication2.cpp
@@ -0,0 +1,32 @@
+// ConsoleApplication2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+struct myStruct {
+	int i{};
+	string s{};
+	bool b{};
+
+	myStruct() {
+		cout << 123 << endl;
+	}
+};
+
+struct point {
+	int x;
+	int y;
+};
+
+int main()
+{
+	struct myStruct test;
+	test.b = false;
+
+	vector<myStruct> blah(5);
+
+	std::cout << "Hello World!\n";
+}
